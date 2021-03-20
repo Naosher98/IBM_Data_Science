6b3f58895386e3abf839e852c46558e068695fba
Naosher98 <65498247+Naosher98@users.noreply.github.com>
2021-03-20 11:02:03 +0600

    Create firstPythonFile.py

---
+++ firstPythonFile.py
@@ -0,0 +1,33 @@
+# PDF FILE COMBINER
+import PyPDF2
+pdf1 = input('Input pdf 1: ')
+print('\n')
+pdf2 = input('Input pdf 2: ')
+print('\n')
+outputPdf = input('Name Outcome Pdf: ')
+def pdf(x):
+	pdfObj= open(x,'rb')
+	pdfReader= PyPDF2.PdfFileReader(pdfObj)
+	return pdfReader
+
+pdfReader1 = pdf(pdf1)
+pdfReader2 = pdf(pdf2)
+# pdfObj1= open(pdf1,'rb')
+# pdfObj2= open(pdf2,'rb')
+# pdfReader1= PyPDF2.PdfFileReader(pdfObj1)
+# pdfReader2= PyPDF2.PdfFileReader(pdfObj2)
+pdfWriter = PyPDF2.PdfFileWriter()
+
+
+for i in range(pdfReader1.numPages):
+	pageObj1=pdfReader1.getPage(i)
+	pdfWriter.addPage(pageObj1)
+
+for i in range(pdfReader2.numPages):
+	pageObj2=pdfReader2.getPage(i)
+	pdfWriter.addPage(pageObj2)
+pdfOutputFile =open(outputPdf,'wb')
+pdfWriter.write(pdfOutputFile)
+pdfOutputFile.close()
+pdfObj1.close()
+pdfObj2.close()
